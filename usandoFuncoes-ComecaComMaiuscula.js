d8c417965148cd1fb196eaaf6f76508f3fcb0f59
mayara_Abreu <mayara.arj@gmail.com>
2022-04-16 16:05:41 -0300

    Criação do exercício

---
+++ usandoFuncoes-ComecaComMaiuscula.js
@@ -0,0 +1,12 @@
+function comecaComMaiuscula(palavra){
+    return /^[A-Z]/.test(palavra);
+ }
+  
+ var palavras = ["Amor", "copo", "Bolacha", "biscoito"];
+  
+ for (i = 0; i < palavras.length; i++){
+   if (comecaComMaiuscula(palavras[i])){
+      console.log("Começa com maiúscula")} else {
+         console.log("Não começa com maiúscula")
+      }
+   }
